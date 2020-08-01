5c1f244ce544ea907a09b4b080326b28c0ddf8d2
Jenshuo <60322917+Jenshuo@users.noreply.github.com>
2020-08-01 15:33:08 +0800

    Delete plot.py

--- Lab2/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-# plt.title("Activation function comparision(EEGNet)")
-# plt.xlabel("Epoch")
-# plt.ylabel("Accuracy")
-# plt.plot(acc_train, label='ELU train')
-# plt.plot(acc_test, label='ELU test')
-# plt.legend(loc='lower right')
-# plt.show()
